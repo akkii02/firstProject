da73f56c66cb96b5a82b799562ef0756a8915118
akshaysable02 <akshaysable097@gmail.com>
2023-07-06 16:54:44 +0530

    Add new li using classname and tagname

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-var headerTitle = document.getElementById('header-title');
-var mainTittle = document.getElementById('main-header');
-var addItems = document.getElementById('title');
-console.log(headerTitle.textContent);
-headerTitle.textContent = 'Hello';
-headerTitle.style.color = 'black';
-console.log(mainTittle)
-mainTittle.style.border = '5px solid black';
-console.log(addItems.textContent)
-addItems.style.color='green';
-addItems.fontWeight = 'Bold';
+// var headerTitle = document.getElementById('header-title');
+// var mainTittle = document.getElementById('main-header');
+// var addItems = document.getElementById('title');
+// console.log(headerTitle.textContent);
+// headerTitle.textContent = 'Hello';
+// headerTitle.style.color = 'black';
+// console.log(mainTittle)
+// mainTittle.style.border = '5px solid black';
+// console.log(addItems.textContent)
+// addItems.style.color='green';
+// addItems.fontWeight = 'Bold';
 
 
 
-var addNewItems = document.getElementsByClassName('list-group-item');
+// var addNewItems = document.getElementsByClassName('list-group-item');
 
-addNewItems[2].style.backgroundColor = 'green';
-for(let i=0;i<addNewItems.length;i++){
-    addNewItems[i].style.fontWeight = 'bold';
-}
+// addNewItems[2].style.backgroundColor = 'green';
+// for(let i=0;i<addNewItems.length;i++){
+//     addNewItems[i].style.fontWeight = 'bold';
+// }
+
+// Using getElementsByClassName
+var items = document.getElementsByClassName('list-group')[0];
+var newLi = document.createElement('li');
+newLi.textContent = 'Item 4';
+items.appendChild(newLi);
+
+var  newItem = document.getElementById('items');
+var newLi5 = document.createElement('li');
+newLi5.textContent = 'Item 6';
+newItem.appendChild(newLi5)
